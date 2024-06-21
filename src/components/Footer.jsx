b9c6044cf0bab791f727c867b1678eff47c30293
Shamil <compshamil@gmail.com>
2024-06-21 12:46:17 -0400

    footer css stuff

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import githubIcon from "../assets/icon/github.svg";
+import linkedin from "../assets/icon/linkedin.svg";
+
+export default function Footer() {
+  return (
+    <div className="flex items-center justify-between py-6 px-16 bg-slate-50 mt-auto">
+      <div>
+        <p className="content-center text-4xl">
+          for<span className="text-[#4DB010]">Sure</span>Hired
+        </p>
+        <p className="text-slate-700 text-xs">© forSure Hired 2024.</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <img src={githubIcon} alt="GitHub Link" />
+        <img src={linkedin} alt="LinkedIn Link" />
+      </div>
+    </div>
+  );
+}
