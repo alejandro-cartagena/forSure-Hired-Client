61e1df2630a7a0e1a85bf3d0ffb838e1eceb960c
Shamil <compshamil@gmail.com>
2024-06-25 14:22:30 -0400

    protected routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -7,16 +7,38 @@ import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
+  const IsLoggedIn = () => {
+    return localStorage.getItem("authToken") ? (
+      <Outlet />
+    ) : (
+      <Navigate to="/login" />
+    );
+  };
+
+  const IsNotLoggedIn = () => {
+    return !localStorage.getItem("authToken") ? (
+      <Outlet />
+    ) : (
+      <Navigate to="/dashboard" />
+    );
+  };
+
   return (
     <div className="bg-slate-200 flex flex-col min-h-[100vh]">
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/dashboard" />
-        <Route path="/ai" />
-        <Route path="/study" />
+
+        <Route element={<IsNotLoggedIn />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Route>
+
+        <Route element={<IsLoggedIn />}>
+          <Route path="/dashboard" />
+          <Route path="/ai" />
+          <Route path="/study" />
+        </Route>
       </Routes>
       <Footer />
     </div>
