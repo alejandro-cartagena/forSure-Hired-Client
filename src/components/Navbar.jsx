f275199a5097f770674ce760563b195b88b44fbb
Renmy <renmyenriquez@gmail.com>
2024-06-26 10:15:45 -0400

    minor fix on nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/images/FSH_logo.png";
 import { AuthContext } from "../context/auth.context";
 
@@ -7,16 +7,19 @@ export default function Navbar() {
   const { user } = useContext(AuthContext);
   return (
     <div className="flex items-center justify-between py-6 md:px-32 px-20 bg-slate-50">
-      <div className="flex items-center gap-2">
-        <img src={logo} alt="For Sure Hire Logo" className="w-16 h-16" />
-
-        <Link to="/">
-          <p className="content-center text-4xl">
-            for<span className="text-green-500">Sure</span>Hired
-          </p>
-        </Link>
-
-      </div>
+      <Link to="/">
+        <div className="flex items-center gap-2">
+          <img src={logo} alt="For Sure Hire Logo" className="w-16 h-16" />
+          <div className="flex flex-col">
+            <p className="content-center text-4xl">
+              for<span className="text-green-500">Sure</span>Hired
+            </p>
+            <span className="text-sm text-slate-400 tracking-[0.15em] text-center">
+              The Ultimate Job Tracker
+            </span>
+          </div>
+        </div>
+      </Link>
       <nav className="flex items-center gap-6 font-semibold text-slate-600">
         {/* TODO: Use Navlink to setup active status */}
         <NavLink
