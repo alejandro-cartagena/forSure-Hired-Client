dda417331b296f3d40b2dc7899d3314734bcf01c
Alejandro <alejandrocartagena68@gmail.com>
2024-06-27 18:11:05 -0400

    Modified landing page and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import githubIcon from "../assets/icon/github.svg";
+import gitIcon from "../assets/icon/git.svg";
 import linkedin from "../assets/icon/linkedin.svg";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className="flex items-center justify-between py-6 md:px-32 px-20 bg-slate-50 mt-auto">
+    <div className="text-[#334155] flex flex-col text-center sm:text-left sm:flex-row items-center justify-between py-6 md:px-32 px-20 bg-slate-50 mt-auto gap-4">
       <div>
         <p className="content-center text-4xl">
           for<span className="text-[#4DB010]">Sure</span>Hired
         </p>
         <p className="text-slate-700 text-xs">© forSure Hired 2024.</p>
       </div>
-      <div className="flex items-center gap-2">
-        <img src={githubIcon} alt="GitHub Link" />
-        <img src={linkedin} alt="LinkedIn Link" />
+      <div className="flex flex-col items-center gap-4">
+        <a
+          href="https://github.com/alejandro-cartagena/forSure-Hired-Client"
+          target="_blank"
+          className="flex items-center gap-2"
+        >
+          <img src={githubIcon} alt="GitHub Link" />
+          <p>Github Repo Frontend</p>
+        </a>
+        <a
+          href="https://github.com/alejandro-cartagena/forSure-Hired-server"
+          target="_blank"
+          className="flex items-center gap-2"
+        >
+          <img src={gitIcon} alt="GitHub Link" />
+          <p>Github Repo Backend</p>
+        </a>
       </div>
     </div>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,25 +8,27 @@ import image13 from "../assets/images/image13.png";
 import image14 from "../assets/images/image14.png";
 
 import jobDescription from "../assets/images/jobDescription.png";
-import generatedQuestions from "../assets/images/generatedQuestions.png";
+import generatedQuestionsImg from "../assets/images/generatedQuestions.png";
+import quizImg from "../assets/images/quiz.png";
+import jobPageImg from "../assets/images/jobPage.png";
 
 export default function Homepage() {
   return (
     <div className="flex flex-col text-[#334155]">
       {/* Hero Section */}
-      <section className="bg-[#F1F5F9] py-12 w-full px-4 sm:px-16 md:px-32 flex flex-col items-center lg:flex-row  lg:justify-between gap-8    ">
+      <section className="bg-[#F1F5F9] py-16 w-full px-4 sm:px-16 md:px-32 flex flex-col items-center lg:flex-row  lg:justify-between gap-8    ">
         <div className="text-center w-full flex flex-col justify-center gap-8 lg:w-1/2">
           <h1 className="text-5xl">
             <span className="text-[#4DB010]">Track Jobs</span> and{" "}
             <span className="text-[#4DB010]">Ace Interviews</span> with
-            AI-Generated Questions
+            AI-Generated Quizzes
           </h1>
-          <p className="text-lg w-full">
+          <p className="text-lg w-full lg:max-w-[500px] lg:mx-auto">
             Easily track your job applications and get customized interview
             questions tailored to each job description.
           </p>
           <Link to="/dashboard">
-            <button className="bg-[#334155] text-white text-2xl py-3 px-5 rounded">
+            <button className="bg-[#334155] text-white text-2xl py-3 px-5 rounded ease-in-out duration-300 hover:bg-[#4DB010] hover:scale-105">
               Get Started
             </button>
           </Link>
@@ -38,7 +40,7 @@ export default function Homepage() {
             className="shadow-lg rounded-md w-full object-cover"
           /> */}
           <img
-            src={generatedQuestions}
+            src={quizImg}
             alt="Image 1"
             className="shadow-lg rounded-md w-full object-cover"
           />
@@ -46,12 +48,12 @@ export default function Homepage() {
       </section>
 
       {/* Section 2 */}
-      <section className="bg-[#DCFFCF] px-4 sm:px-16 md:px-32 py-12 flex flex-col items-center">
-        <div className="flex flex-col items-center py-12 justify-between">
+      <section className="bg-[#DCFFCF] px-4 sm:px-16 md:px-32 py-16 flex flex-col items-center">
+        <div className="flex flex-col items-center justify-between gap-8">
           <h1 className="text-5xl text-center">
             Keep track of Job Applications
           </h1>
-          <div className="w-full py-12 flex justify-center">
+          <div className="w-full flex justify-center">
             <img
               src={dashboarddemo}
               alt="Image 1"
@@ -67,30 +69,38 @@ export default function Homepage() {
       </section>
 
       {/* Section 3 */}
-      <section className="bg-[#FFFFFF] py-12 w-full px-4 sm:px-16 md:px-32 flex flex-col items-center lg:flex-row  lg:justify-between gap-8">
-        <div className="w-full text-center order:last flex flex-col justify-center gap-8 md:order-first lg:w-1/2">
+      <section className="bg-[#FFFFFF] py-16 w-full px-4 sm:px-16 md:px-32 flex flex-col items-center lg:flex-row  lg:justify-between gap-8">
+        <div className="w-full text-center order:last flex flex-col justify-center gap-8 lg:order-first lg:w-1/2">
           <h1 className="text-5xl">Job Overview</h1>
-          <p className="text-lg">
+          <p className="text-lg w-full lg:max-w-[500px] lg:mx-auto">
             Access a dedicated page to view all your saved jobs in a clean,
             two-column layout similar to popular job sites like Indeed.
             Effortlessly search and filter to find specific jobs.
           </p>
         </div>
-        <div className="w-1/2 py-12">
-          <img src={image13} alt="Image 13" className="mx-auto" />
+        <div className="w-full flex justify-center lg:w-1/2 gap-4">
+          <img
+            src={jobPageImg}
+            alt="Image 13"
+            className="shadow-lg rounded-md w-full object-cover mx-auto"
+          />
         </div>
       </section>
 
       {/* Section 4 */}
-      <section className="bg-[#F1F5F9] py-12 w-full px-4 sm:px-16 md:px-32 flex flex-col items-center md:flex-row  md:justify-between gap-8">
-        <div className="md:order-first order-last w-1/2 py-12">
-          <img src={image13} alt="Image 13" className="mx-auto" />
+      <section className="bg-[#F1F5F9] py-16 w-full px-4 sm:px-16 md:px-32 flex flex-col items-center lg:flex-row  lg:justify-between gap-8">
+        <div className="w-full flex justify-center lg:w-1/2 gap-4 order-last lg:order-first">
+          <img
+            src={generatedQuestionsImg}
+            alt="Image 13"
+            className="shadow-lg rounded-md w-full object-cover mx-auto"
+          />
         </div>
-        <div className="w-full text-center order:last flex flex-col justify-center gap-8 md:order-first md:text-left md:w-1/2">
+        <div className="w-full text-center order:last flex flex-col justify-center gap-8 lg:w-1/2">
           <h1 className="text-5xl">
-            Prepare with Tailored Interview Questions
+            Prepare with Interview Questions & Answers
           </h1>
-          <p className="text-lg">
+          <p className="text-lg w-full lg:max-w-[500px] lg:mx-auto">
             Generate customized behavioral and technical interview questions
             based on job descriptions using our AI feature. Take quizzes based
             on the questions, receive detailed feedback, and save them in your
@@ -100,15 +110,15 @@ export default function Homepage() {
       </section>
 
       {/* Section 5 */}
-      <section className="bg-[#DCFFCF] py-28 px-20 w-full">
-        <div className="container flex flex-col items-center w-full gap-12">
-          <p className="text-center text-2xl w-full md:max-w-[700px]">
+      <section className="bg-[#DCFFCF] py-28 px-4 sm:px-16 md:px-32 w-full">
+        <div className="flex flex-col items-center w-full gap-12">
+          <p className="text-center text-2xl w-full lg:max-w-[700px]">
             forSure Hired is your all-in-one solution to keep track of your job
             applications and excel in your interviews. Join us and take control
             of your job search journey!
           </p>
           <Link to="/login">
-            <button className="bg-[#334155] text-white text-2xl py-3 px-5 rounded">
+            <button className="bg-[#334155] text-white text-2xl py-3 px-5 rounded ease-in-out duration-300 hover:bg-[#4DB010] hover:scale-105">
               Sign up Now
             </button>
           </Link>
